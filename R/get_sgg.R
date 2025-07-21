2773ebdd544c4546ddc3c3a788ea1ba697e22e75
Derick S. Park <145691757+derickspark@users.noreply.github.com>
2025-07-21 13:17:12 +0900

    Create get_sgg.R

---
+++ R/get_sgg.R
@@ -0,0 +1,10 @@
+#' 시군구 경계(shp) 불러오기
+#' @return sf 객체
+#' @export
+get_sgg <- function() {
+  zip_path <- system.file("extdata", "sgg.zip", package = "admin_map_KR")
+  tmp <- tempfile()
+  unzip(zip_path, exdir = tmp)
+  shp_file <- list.files(tmp, pattern = "\\.shp$", full.names = TRUE)[1]
+  sf::st_read(shp_file, quiet = TRUE)
+}
