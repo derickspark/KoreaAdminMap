b98159b8d0686317bfbba8e003ba877e885e2c10
Derick S. Park <145691757+derickspark@users.noreply.github.com>
2025-07-21 14:05:17 +0900

    Update get_sd.R

--- R/get_sd.R
+++ R/get_sd.R
@@ -2,6 +2,6 @@
 #' @return sf 객체
 #' @export
 get_sd <- function() {
-  gpkg_path <- system.file("extdata", "sd.gpkg", package = "admin_map_KR")
+  gpkg_path <- system.file("extdata", "sd.gpkg", package = "adminmapKR")
   sf::st_read(gpkg_path, layer = "sd", quiet = TRUE)
 }
