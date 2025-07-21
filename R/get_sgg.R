2f9a4c3269e4a9bf409a4157ced1413bd0e90e56
Derick S. Park <145691757+derickspark@users.noreply.github.com>
2025-07-21 14:05:51 +0900

    Update get_sgg.R

--- R/get_sgg.R
+++ R/get_sgg.R
@@ -2,6 +2,6 @@
 #' @return sf 객체
 #' @export
 get_sgg <- function() {
-  gpkg_path <- system.file("extdata", "sgg.gpkg", package = "admin_map_KR")
+  gpkg_path <- system.file("extdata", "sgg.gpkg", package = "KoreaAdminMap")
   sf::st_read(gpkg_path, layer = "sgg", quiet = TRUE)
 }
